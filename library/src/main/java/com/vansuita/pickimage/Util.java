37986409a7afacbefc5505857adefb87d0d08d9e
jrvansuita <vansuita.jr@gmail.com>
2017-01-12 10:39:08 -0500

    Real Path fix.

--- library/src/main/java/com/vansuita/pickimage/Util.java
+++ library/src/main/java/com/vansuita/pickimage/Util.java
@@ -132,11 +132,13 @@ public class Util {
     public static String getRealPathFromURI(Context context, Uri contentUri) {
         Cursor cursor = null;
         try {
-            String[] proj = { MediaStore.Images.Media.DATA };
-            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
             int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             cursor.moveToFirst();
             return cursor.getString(column_index);
+        }catch (Exception e){
+            return contentUri.getPath();
         } finally {
             if (cursor != null) {
                 cursor.close();
